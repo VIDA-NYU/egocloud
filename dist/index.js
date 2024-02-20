ed33c5f76bbfa475ef1974c643e8bb63de8af074
Suemy Inagaki <Inagakisuemy@gmail.com>
2024-02-20 13:56:29 -0500

    remove js files

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Projector = exports.Dataset = exports.SceneViewer = void 0;
-var SceneViewer_1 = require("./SceneViewer");
-Object.defineProperty(exports, "SceneViewer", { enumerable: true, get: function () { return SceneViewer_1.SceneViewer; } });
-var Dataset_1 = require("./model/Dataset");
-Object.defineProperty(exports, "Dataset", { enumerable: true, get: function () { return Dataset_1.Dataset; } });
-var Projector_1 = require("./utils/Projector");
-Object.defineProperty(exports, "Projector", { enumerable: true, get: function () { return Projector_1.Projector; } });
